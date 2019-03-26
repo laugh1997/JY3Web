dc75a0c470e89e5e77fd6e5a426fa4b00ea3dcdb
laugh1997 <48004715+laugh1997@users.noreply.github.com>
2019-03-26 20:08:09 +0800

    Add files via upload

---
+++ Defence items.js
@@ -0,0 +1,42 @@
+$("#container").css("background-color","white");  
+//这里是带有下拉条最大的DIV
+$("#container1").css({"background-color":"#CCCCCC","display":"flex"});
+$("#container1").css("align-items","center");
+$("#container1").append("<img src='../img/10.7515715.svg' />")
+$("#container img").css({"width":"80px","height":"80px","margin-left":"10px"});
+$("#container1").append("<span>了解及购买</span>");
+$("#container1 span").css({"margin-left":"190px","color":"white","font-size":"17px"});
+//这里是最上面的DIV 带有睿乐购图标的那个
+$("#container2").css("display","flex");
+$("#container2").css("align-items","center");
+$("#container2").css("background-color","#666666");
+$("#container2").append("<span>Ghibli 卓尔不凡</span>");
+$("#container2 span").css("margin-left","17px");
+$("#container2 span").css({"color":"white","font-size":"17px","font-weight":"1000"});
+//这里是第二个DIV 里面带有文字的
+$("#container3").css({"width":"100%","height":"200px","border":"1px solid black"});
+$("#container3").append("<img src='' />")
+//这里是轮播图
+$("#container4").css({"width":"100%","height":"50px","background-color":"#666666","display":"flex"});
+$("#container4").append("<span>GranTurismo143.9万RMB起</span>");
+$("#container4").css({"justify-content":"center","align-items":"center"});
+$("#container4 span").css({"color":"white","font-size":"17px","font-weight":"1000"});
+//这里是轮播图下面的那个价格
+$("#container5").css({"width":"100%","height":"200px"});
+$("#container5").append("<img src='../img/ghibli.jpg'/>");
+$("#container5 img").css({"width":"100%","height":"200px"});
+//第一张静态图片 蓝色的玛莎拉蒂
+$("#container6").css({"width":"100%","height":"50px","background-color":"#666666","display":"flex"});
+$("#container6").append("<span>Quattroporte总裁219万RMB</span>");
+$("#container6").css({"justify-content":"center","align-items":"center"});
+$("#container6 span").css({"color":"white","font-size":"17px","font-weight":"1000"});
+//这里是蓝色的玛莎拉蒂的价格 中间灰色的DIV
+
+
+
+
+
+
+
+
+
